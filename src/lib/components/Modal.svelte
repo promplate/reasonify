e27aba3fa3a3b6bd280d6cd6839419e925957d56
Muspi Merol <wjj123345567@icloud.com>
2024-03-24 21:12:57 +0800

    feat(ui): add `Model`

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let show: boolean;
+
+  let display = show;
+
+  let timeoutId: NodeJS.Timeout;
+
+  $: if (show) {
+    display = true;
+    clearTimeout(timeoutId);
+  }
+  else {
+    timeoutId = setTimeout(() => {
+      display = false;
+    }, 700);
+  }
+</script>
+
+{#if display}
+  <slot name="backdrop">
+    <div class="pointer-events-none fixed inset-0 transition duration-600" class:show />
+  </slot>
+  <div class="fixed inset-0 grid place-items-center transition duration-300" class:op-0={!show}>
+    <slot name="inner">
+      <div class="rounded bg-white/2 p-4 ring-1.3 ring-white/20">
+        <slot />
+      </div>
+    </slot>
+  </div>
+{/if}
+
+<style>
+  div.show {
+    --uno: bg-neutral-9/50;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
-  import type { PyodideInterface } from "pyodide";
-
   import LoadingItem from "./LoadingItem.svelte";
+  import Modal from "$lib/components/Modal.svelte";
   import getPy from "$lib/py";
   import { promplateReady, pyodideReady, reasonifyReady } from "$lib/stores";
   import { onMount } from "svelte";
 
-  let py: PyodideInterface;
-
-  onMount(async () => {
-    py = await getPy();
-  });
+  onMount(() => void getPy());
 </script>
 
-<ol class="fixed top-1/2 flex flex-col gap-2.5 -translate-1/2">
-  <li><LoadingItem loading={!$pyodideReady} text="Loading pyodide runtime" /></li>
-  <li class:op-30={!$pyodideReady}><LoadingItem loading={!$promplateReady} text="Patching promplate" /></li>
-  <li class:op-30={!$promplateReady}><LoadingItem loading={!$reasonifyReady} text="Building headless agent" /></li>
-</ol>
+<Modal show={!$reasonifyReady}>
+  <ol class="flex flex-col gap-2.5 whitespace-nowrap">
+    <li><LoadingItem loading={!$pyodideReady} text="Loading pyodide runtime" /></li>
+    <li class:op-30={!$pyodideReady}><LoadingItem loading={!$promplateReady} text="Patching promplate" /></li>
+    <li class:op-30={!$promplateReady}><LoadingItem loading={!$reasonifyReady} text="Building headless agent" /></li>
+  </ol>
+</Modal>
