5f93818b15a86629156d8a46fe6d045ce8e45f55
Muspi Merol <wjj123345567@icloud.com>
2024-05-10 17:29:09 +0800

    fix: suppress type error when optional env not specified

--- src/lib/py/load.ts
+++ src/lib/py/load.ts
@@ -8,7 +8,7 @@ import * as env from "$env/static/public";
 import { cacheSingleton } from "$lib/utils/cache";
 import { version } from "pyodide";
 
-const indexURL = env.PUBLIC_PYODIDE_INDEX_URL?.replace("{}", version) ?? `https://cdn.jsdelivr.net/pyodide/v${version}/full/`;
+const indexURL = ("PUBLIC_PYODIDE_INDEX_URL" in env) ? (env.PUBLIC_PYODIDE_INDEX_URL as string).replace("{}", version) : `https://cdn.jsdelivr.net/pyodide/v${version}/full/`;
 
 export const getPy = cacheSingleton(async () => {
   const { loadPyodide } = await import("pyodide");
