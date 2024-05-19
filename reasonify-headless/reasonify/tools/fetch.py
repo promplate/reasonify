debd14b7bf51d5d27f40cb1a7d2111be10c7ad2e
Muspi Merol <wjj123345567@icloud.com>
2024-05-20 00:57:44 +0800

    feat(tool): implement `fetch` tool

---
+++ reasonify-headless/reasonify/tools/fetch.py
@@ -0,0 +1,60 @@
+from functools import cache
+from re import MULTILINE
+from re import compile as re_compile
+
+from js import HTMLDivElement, HTMLTemplateElement, document
+from pyodide.http import pyfetch
+
+from ..utils.tool import tool
+
+pattern = re_compile(r"\n{3,}")
+
+
+class FetchFailed(Exception): ...
+
+
+@tool
+async def fetch(url: str) -> tuple[int, str]:
+    """
+    fetch the content of a web page,
+    returns its status code and text content.
+    """
+
+    try:
+        return await _fetch(url)
+    except OSError as e:
+        raise FetchFailed(*e.args) from None
+
+
+async def _fetch(url: str):
+    try:
+        res = await pyfetch(url)
+    except OSError:
+        res = await pyfetch(f"/api/proxy?url={url}")
+
+    content = await res.text()
+
+    return res.status, post_process(purify(content))
+
+
+@cache
+def get_root_node():
+    template: HTMLTemplateElement = document.createElement("template")  # type: ignore
+    return template.content
+
+
+def purify(html: str):
+    div: HTMLDivElement = get_root_node().appendChild(document.createElement("div"))  # type: ignore
+    div.innerHTML = html
+    return div.innerText
+
+
+sub_traling_spaces = re_compile(r"[ \t]+$", MULTILINE)
+sub_multiple_lines = re_compile(r"\n{4,}")
+
+
+def post_process(text: str):
+    text = text.replace("\r", "")
+    text = sub_traling_spaces.sub("", text)
+    text = sub_multiple_lines.sub("\n\n\n", text)
+    return text
