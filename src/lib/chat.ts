cc1d5b09de32f92610bbf94284282b8d91a0b2a5
Muspi Merol <wjj123345567@icloud.com>
2024-08-24 03:08:34 +0800

    chore: raise error when fail to chat with LLM

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -4,5 +4,9 @@ import { responseToAsyncIterator } from "./utils/iter";
 
 export default async function* (options: ChatCompletionCreateParams) {
   const res = await fetch("/api/chat", { method: "POST", body: JSON.stringify(options), headers: { "content-type": "application/json" } });
+  if (res.status !== 200) {
+    console.error(await res.text());
+    throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
+  }
   yield * responseToAsyncIterator(res);
 }
