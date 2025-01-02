93ea3491468a86b091cb50bb005ddf4a61d11dad
Muspi Merol <me@promplate.dev>
2025-01-02 19:55:14 +0800

    chore: remove unused utilities

--- reasonify-headless/reasonify/templates/__init__.py
+++ reasonify-headless/reasonify/templates/__init__.py
@@ -1,20 +1,10 @@
-from json import dumps
 from pathlib import Path
 
 from promplate_recipes import DotTemplate
-from promplate_recipes.context import layers, register_components
-
-from ..utils.serialize import RobustEncoder
+from promplate_recipes.context import register_components
 
 root = Path(__file__).parent
 
 register_components(root)
-
-utilities = {
-    "json": lambda obj: dumps(obj, indent=4, ensure_ascii=False, cls=RobustEncoder),
-    "join": lambda parts: (parts[0] if len(parts) < 2 else ", ".join(parts[:-1]) + f" and {parts[-1]}"),
-}
-
-layers.append(utilities)
 
 main = DotTemplate.read(root / "main.j2")
