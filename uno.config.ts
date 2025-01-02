36be18ac964ec38dd899e8f0d9499850faa51d59
Muspi Merol <me@promplate.dev>
2025-01-02 20:22:03 +0800

    feat: enable on-demand preflight feature of the new UnoCSS

--- uno.config.ts
+++ uno.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, presetIcons, presetTypography, presetUno, presetWebFonts,
 const config = defineConfig({
   extractors: [extractorSvelte()],
   transformers: [transformerVariantGroup(), transformerDirectives()],
-  presets: [presetUno(), presetWebFonts({ provider: "bunny", fonts: {
+  presets: [presetUno({ preflight: "on-demand" }), presetWebFonts({ provider: "bunny", fonts: {
     mono: { name: "JetBrains Mono Variable", provider: "none" },
     fira: { name: "Fira Code Variable", provider: "none" },
     fancy: "Archivo Black",
