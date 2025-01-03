5e97a88e804510ad811c1e00b33a56c7122cdd37
Muspi Merol <me@promplate.dev>
2025-01-03 19:12:35 +0800

    fix: update regex for dependency parsing to allow CRLF

--- src/lib/py/requirements.ts
+++ src/lib/py/requirements.ts
@@ -1,7 +1,7 @@
 import code from "$lib/../../pyproject.toml?raw";
 
 function getDependencies(toml: string) {
-  return JSON.parse(/dependencies\s*=\s*(\[[\s\S]*?\])\n/.exec(toml)![1].replace(",\n]", "]")) as string[];
+  return JSON.parse(/dependencies\s*=\s*(\[[\s\S]*?\])\r?\n/.exec(toml)![1].replace(/,\r?\n\]/, "]")) as string[];
 }
 
 export default getDependencies(code);
