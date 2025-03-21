f4bf6c92398a66a4bd369a94aa20a1d64b45166b
Muspi Merol <me@promplate.dev>
2025-03-21 22:54:08 +0800

    fix: prevent memory leaks in `clearApiCache`

--- src/lib/py/api.ts
+++ src/lib/py/api.ts
@@ -1,17 +1,20 @@
+import type { PyCallable, PyProxy } from "pyodide/ffi";
+
 import { py } from "./load";
 
-const cache = new Map<string, any>();
+const cache = new Map<string, PyProxy>();
 
 export function getApi<T = () => any>(slug: string): T {
   if (cache.has(slug)) {
     return cache.get(slug) as T;
   } else {
-    const api = py.pyimport(slug) as T;
+    const api = py.pyimport(slug) as T & PyCallable;
     cache.set(slug, api);
     return api;
   }
 }
 
 export function clearApiCache() {
+  cache.forEach(value => value.destroy());
   cache.clear();
 }
