0e84697c7a46508b5731cdee310d9fc4ee2b4e76
Muspi Merol <me@promplate.dev>
2025-11-21 03:38:54 +0800

    feat: specify `optimizeDeps` in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [Unocss(), sveltekit(), flattenDir()],
+  optimizeDeps: { entries: ["index.html", "**/*.svelte", "src/routes/**/*.ts", "!**/*server.ts"] },
 });
